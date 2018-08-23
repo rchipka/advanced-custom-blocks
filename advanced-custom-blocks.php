5d647a0672b1d4d84a540e4516a78d22082a2828
Robbie Chipka <chipka01@email.franklin.edu>
2018-08-23 19:13:20 -0400

    Update advanced-custom-blocks.php

--- advanced-custom-blocks.php
+++ advanced-custom-blocks.php
@@ -4,18 +4,18 @@
 * Plugin Name: Advanced Custom Blocks
 * Plugin URI: https://github.com/rchipka/advanced-custom-blocks
 * Description: ACF for Gutenberg blocks
-* Version: 1.0.0
+* Version: 2.0.0
 * Author: Robbie Chipka
 * Author URI: https://github.com/rchipka
 * GitHub Plugin URI: https://github.com/rchipka/advanced-custom-blocks
 */
 
 add_filter('acf/location/rule_types', function ( $choices ) {
-    $choices['gutenberg']['block_type'] = 'Block Type';
+    $choices['Gutenberg']['block_name'] = 'Block Name';
     return $choices;
 }, 10, 1);
 
-add_filter('acf/location/rule_values/block_type', function ( $choices, $data ) {
+add_filter('acf/location/rule_values/block_name', function ( $choices, $data ) {
     return
       '<input type="text" data-ui="0" data-ajax="0" data-multiple="0" data-allow_null="0" ' .
         'id="acf_field_group-location-' . $data['group'] . '-' . $data['id'] . '-value" ' .
@@ -23,322 +23,332 @@ add_filter('acf/location/rule_values/block_type', function ( $choices, $data ) {
         'value="' . $data['value'] . '" />';
 }, 10, 2);
 
-add_filter('acf/location/rule_match/block_type', function ( $match, $rule, $options ) {
+add_filter('acf/location/rule_match/block_name', function ( $match, $rule, $options ) {
   if (empty($rule['value'])) {
     return true;
   }
 
-  return ($rule['operator'] === '==') === ($_REQUEST['block']['name'] == $rule['value']);
+  return ($rule['operator'] === '==') === ($GLOBALS['ACF_BLOCK_NAME'] == $rule['value']);
 }, 10, 3);
 
-add_filter('acf/load_field', function ($field) {
-  $fields = $_REQUEST['acf'] ?: $_REQUEST['block']['attributes']['acf_fields'];
+add_filter('acf/location/screen', function ($screen, $field_group) {
+  if (!$field_group['block_name']) {
+    return $screen;
+  }
 
-  if (!$_REQUEST['isgutenberg'] || !$fields[$field['key']]) {
+  if (!$screen['post_id']) {
+    $screen['post_id'] = $_REQUEST['post'] ?: $_REQUEST['post_id'] ?: $_REQUEST['attributes']['post_id'];
+  }
+
+  return $screen;
+}, 1, 2);
+
+add_action('save_post', function ($post_id) {
+  if (!$_POST['acf_blocks']) {
+    return;
+  }
+
+  // error_log(print_r($_POST['acf_blocks'], 1));
+
+  foreach ($_POST['acf_blocks'] as $block_id => $fields) {
+    foreach ($fields as $key => $value) {
+      update_post_meta($post_id, $key . '_' . $block_id, $value);
+    }
+  }
+}, 10, 1);
+
+add_action('acf/render_field_group_settings', function ($field_group) {
+  acf_render_field_wrap(array(
+    'label'     => 'Block Name',
+    'instructions'  => '',
+    'type'      => 'text',
+    'name'      => 'block_name',
+    'prefix'    => 'acf_field_group',
+    'value'     => $field_group['block_name'],
+  ));
+
+  acf_render_field_wrap(array(
+    'label'     => 'Block Icon',
+    'instructions'  => '',
+    'type'      => 'text',
+    'name'      => 'block_icon',
+    'prefix'    => 'acf_field_group',
+    'value'     => $field_group['block_icon'],
+  ));
+}, 10, 1);
+
+add_filter('acf/load_field', function ($field) {
+  if (!$GLOBALS['ACF_BLOCK_ID'] || !$GLOBALS['ACF_POST_ID']) {
     return $field;
   }
 
-  $field['value'] = $fields[$field['key']];
+  $value = get_post_meta($GLOBALS['ACF_POST_ID'], $field['key'] . '_' . $GLOBALS['ACF_BLOCK_ID'], true);
+
+  if ($value) {
+    $field['value'] = $value;
+  }
+
+  // error_log('load field ' . $field['name']. $field['key'] . '_' . $GLOBALS['ACF_BLOCK_ID'] . print_r($value, 1));
 
   return $field;
 }, 10, 1);
 
-add_action('wp_ajax_gutenberg_match_acf', function () {
-  global $post;
 
-  $fields = $_REQUEST['acf'] ?: $_REQUEST['block']['attributes']['acf_fields'];
+// add_filter('acf/load_value', function ($value, $post_id, $field) {
+//   error_log('load_field'. json_encode($field['name']));
+//   if (!$GLOBALS['ACF_BLOCK_ID']) {
+//     return $value;
+//   }
 
-  $response = array();
+//   error_log($post_id . $field['key'] . '_' . $GLOBALS['ACF_BLOCK_ID']);
+//   return get_post_meta($post_id, $field['key'] . '_' . $GLOBALS['ACF_BLOCK_ID'], true) ?: $value;
+// }, 10, 3);
 
-  if ($_REQUEST['post_id']) {
-    setup_postdata($post = get_post($_REQUEST['post_id']));
-  }
 
-  foreach ( acf_get_field_groups() as $field_group ) {
-    if (acf_get_field_group_visibility($field_group)) {
-      $field_group['fields'] = acf_get_fields($field_group) ?: [];
-
-      if ($field_group['fields']) {
-        error_log(json_encode($_REQUEST));
-        foreach ($field_group['fields'] as &$field) {
-          $field['value'] = $fields[$field['key']];
-        }
-
-        ob_start();
-        acf_render_fields($field_group['fields']);
-        $field_group['html'] = ob_get_contents();
-        ob_end_clean();
-      } else {
-        $field_group['html'] = '';
-      }
-
-      $response[] = $field_group;
-    }
-  }
-
-  wp_send_json($response);
-});
-
-function recursive_unset(&$array, $unwanted_key) {
-  if ($array[$unwanted_key]) {
-    $value = $array[$unwanted_key];
-    unset($array[$unwanted_key]);
-    array_push($array, $value);
-  }
-
-  foreach ($array as &$value) {
-    if (is_array($value)) {
-      recursive_unset($value, $unwanted_key);
-    }
-  }
-}
-
-add_filter('gutenberg/template/block_attributes', function ($attributes, $block) {
-  global $post;
-
-  if ($_REQUEST['post_id']) {
-    setup_postdata($post = get_post($_REQUEST['post_id']));
-  }
-
-  if (!($attributes['acf_fields'] = $_REQUEST['acf'])) {
-    return $attributes;
-  }
-
-  $fields = json_decode(preg_replace_callback(
-    '/"([^"]+)"\:/',
-    function ($matches) {
-      $key = $matches[1];
-
-      $field = get_field_object($key);
-
-      if ($field) {
-        return '"' . $field['name'] . '":';
-      }
-
-      return '"_remove_key":';
-    }, json_encode($_REQUEST['acf'])), true);
-
-  if (is_array($fields)) {
-    recursive_unset($fields, '_remove_key');
-    foreach ($fields as $key => $value) {
-      $attributes['acf_fields'][$key] = $value;
-    }
-  }
-
-  return $attributes;
-}, 10, 3);
-
-add_filter('acf/block_content/type=core/paragraph', function ($content) {
-  error_log($content);
-  return preg_replace('/test/i', 'YARP', $content);
-});
-
-add_action('admin_footer', function () {
-  if (!get_the_ID()) {
+add_action( 'init', function () {
+  if (!function_exists('register_block_type')) {
     return;
   }
+
+  foreach (acf_gb_get_block_field_groups() as $group) {
+    register_block_type( 'acf/' . $group['block_name'], array(
+      'attributes'      => array(
+        'acf_fields' => array(
+          'type'    => 'string',
+          'default' => '',
+        ),
+        'post_id' => array(
+          'type'    => 'number',
+          'default' => 0,
+        ),
+        'block_id' => array(
+          'type'    => 'string',
+          'default' => '',
+        ),
+        'block_name' => array(
+          'type'    => 'string',
+          'default' => $group['block_name'],
+        ),
+        'acf_field_group' => array(
+          'type'    => 'string',
+          'default' => '',
+        ),
+      ),
+      'render_callback' => function ($attributes) {
+        global $post;
+
+        $output = '';
+
+        if ($attributes['post_id']) {
+          setup_postdata($post = get_post($attributes['post_id']));
+        }
+
+        $cache_active = acf_is_cache_active();
+
+        if ($cache_active) {
+          acf_disable_cache();
+        }
+
+        $field_group = json_decode($attributes['acf_field_group'], true);
+
+        $GLOBALS['ACF_POST_ID'] = $attributes['post_id'];
+        $GLOBALS['ACF_BLOCK_ID'] = $attributes['block_id'];
+        $GLOBALS['ACF_BLOCK_NAME'] = $attributes['block_name'];
+
+        if ($_GET['context'] === 'edit') {
+          ob_start();
+          $fields = acf_get_fields($field_group);
+          acf_render_fields($fields, $attributes['post_id']);
+          $output .= ob_get_contents();
+          ob_end_clean();
+        } else {
+          $output = apply_filters('acf/render_block', $output, $attributes);
+          $output = apply_filters('acf/render_block/name=' . $attributes['block_name'], $output, $attributes);
+        }
+
+        reset_rows();
+        wp_reset_postdata();
+        $GLOBALS['ACF_BLOCK_ID'] = null;
+        $GLOBALS['ACF_BLOCK_NAME'] = null;
+
+        if ($cache_active) {
+          acf_enable_cache();
+        }
+        
+
+        return $output;
+      },
+    ) );
+  }
+});
+
+function acf_gb_get_block_field_groups() {
+  $groups = [];
+
+  if ($_REQUEST['post']) {
+    setup_postdata($post = get_post($_REQUEST['post']));
+  }
+
+  foreach (acf_get_field_groups() as $group) {
+    $GLOBALS['ACF_BLOCK_NAME'] = $is_block = ($group['block_name']);
+
+    if ($is_block) {
+
+      if (acf_get_field_group_visibility($group)) {
+        $groups[] = $group;
+      }
+    }
+
+    $GLOBALS['ACF_BLOCK_NAME'] = null;
+  }
+
+  if ($_REQUEST['post']) {
+    wp_reset_postdata();
+  }
+
+  return $groups;
+}
+
+add_action('admin_notices', function () {
   ?>
 <script>
 (function ($) {
+  if (!window.wp || !window.wp.blocks || !window.wp.editor) {
+    return;
+  }
+
   var groupElements = {},
       fieldGroups = {},
-      fieldGroupForms = {};
+      fieldGroupForms = {},
+      field_groups = <?php echo json_encode(acf_gb_get_block_field_groups()); ?>;
 
-  wp.hooks.addFilter('blocks.registerBlockType', 'acf', function (settings, name) {
-    settings.attributes.acf_fields = { type: 'object' };
-    return settings;
-  }, 10);
+  console.log(field_groups);
 
-  $(document).on('change', '.acf-form[data-block-id]', function () {
-    var id = $(this).attr('id'),
-        self = this,
-        element = groupElements[id];
-
-    console.log('got change', this);
-
-    setTimeout(function () {
-      if (element && element.props) {
-        element.props.setAttributes({ _update: Math.random() });
-      }
-    });
-  });
-
-  wp.hooks.addFilter('blocks.template.ajaxRequest', 'acf', function (data, props, attributes) {
-    console.log('ajaxRequest', attributes);
-    if (!attributes.block_id) {
-      return data;
-    }
-
-    var forms = $('.acf-form[data-block-id="' + attributes.block_id + '"]');
-    
-    var params = acf.serialize(forms);
-
-    if (params && Object.keys(params).length > 0) {
-      for (var key in params) {
-        data[key] = params[key];
-      }
-    }
-
-    return data;
-  }, 10);
-
-  wp.hooks.addFilter('blocks.BlockEdit', 'acf', wp.element.createHigherOrderComponent(function (element) {
-    return function (props) {
-      var block = wp.hooks.applyFilters(
-            'blocks.template.BlockElement',
-            wp.element.createElement(element, props, wp.element.createElement(wp.blocks.BlockEdit, props)),
-            props),
-          block_id = props.id,
-          attributes = props.attributes;
-
-      if (!props.isSelected) {
-        return block;
-      }
-
-      attributes.block_id = block_id;
-
-      wp.hooks.doAction('blocks.template.createElement', block, props, attributes);
-
-      return block;
-    };
-  }));
-
-  wp.hooks.addFilter('blocks.template.BlockElement', 'acf', function (element, props) {
-    var block_id = props.id;
-
-    console.log('acf block', props);
-    if (!props.isSelected) {
-      return element;
-    }
-    
-    return loadACF(props.name, props, function (groups, justLoaded) {
-      console.log('got groups', groups);
-
-      if (groups.length < 1) {
-        return;
-      }
-
-      var toolbar = [],
-          before = [],
-          after = [],
-          side = []
-          index = 0;
-
-      groups.forEach(function (group) {
-        if (group.position === 'acf_after_title') {
-          before.push(group);
-        } else if (group.position === 'normal') {
-          after.push(group);
-        } else if (group.position === 'toolbar') {
-          toorbar.push(group);
-        } else {
-          side.push(group);
-        }
-      });
-
-      var mapToFormElements = function (array, parent) {
-        if (array.length < 1) {
-          return null;
-        }
-
-        for (var i = 0; i < array.length; i++) {
-          var group = array[i];
-          var id = ['acf', group.ID, index++, block_id].join('-');
-
-          fieldGroups[id] = group;
-          groupElements[id] = element;
-
-          if (!fieldGroupForms[id]) {
-            fieldGroupForms[id] = $('<form>').html(group.html);
-          }
-
-          array[i] = wp.element.createElement('div', {
-            id: id,
-            className: 'acf-form',
-            'data-block-id': block_id,
-            // dangerouslySetInnerHTML: {
-            //   __html: group.html
-            // }
-          });
-
-          if (typeof parent === 'function') {
-            array[i] = parent(group, array[i]);
-          }
-        }
-
-        return array;
-      }
-
-      setTimeout(function () {
-        $('.acf-form[data-block-id="' + block_id + '"]').each(function () {
-          var self = this,
-              id = $(this).attr('id'),
-              form = fieldGroupForms[id];
-
-          if (form && $(this).children()[0] !== form[0]) {
-            $(this).html('');
-            $(this).append(form.detach());
-
-            setTimeout(function () {
-              acf.do_action('ready', $(self));
-            });
-          }
-        });
-
-        if (justLoaded) {
-          element.props.setAttributes({ _update: Math.random() });
-        }
-      });
-
-      return [
-        wp.element.createElement(
-          wp.blocks.BlockControls,
-          {},
-          mapToFormElements(toolbar)
-        ),
-        wp.element.createElement(
-          wp.blocks.InspectorControls,
-          {},
-          mapToFormElements(side, function (group, element) {
-            return wp.element.createElement(
-              wp.components.PanelBody,
-              {
-                title: group.title,
-              },
-              element
-            );
-          })
-        ),
-        mapToFormElements(before),
-        element,
-        mapToFormElements(after),
-      ];
-    }) || element;
-  });
-
-  function loadACF(id, block, callback) {
-    if (fieldGroups.hasOwnProperty(id)) {
-      return callback(fieldGroups[id], false);
-    }
-
-    fieldGroups[id] = [];
-
+  function loadACF(callback) {
     $.ajax({
       url: ajaxurl,
       method: 'POST',
       data: $.param({
         action: 'gutenberg_match_acf',
         post_id: <?php echo json_encode(get_the_ID()); ?>,
-        block: wp.hooks.applyFilters('blocks.template.serializeProps', block),
-      }) + '&' + $.param(block.attributes.acf_fields || {}),
+      }),
       success: function (field_groups) {
-        callback(fieldGroups[id] = field_groups.sort(function (a, b) {
+        callback(field_groups.sort(function (a, b) {
           return a.menu_order - b.menu_order;
         }), true);
       }
     });
   }
+
+  wp.apiFetch.use(function (options, next) { 
+    if (options.path && /block-renderer\/acf/.test(options.path)) {
+      var res = next(options);
+
+      res.then(function () {
+        setTimeout(function () {
+          $('[data-block-id]').each(function () {
+            acf.do_action('ready', $(this));
+          });
+        }, 500);
+      });
+
+      return res;
+    }
+
+    if (options.method !== 'POST' || !(options.body instanceof FormData)) {
+      return next(options);
+    }
+
+    $('.acf-block-group-content').each(function () {
+      var form = this;
+
+      // var data = $.param(acf.serialize($(this)));
+      // console.log(data);
+
+      (new FormData(this)).forEach(function (val, key) {
+        // var val = data[key];
+        console.log('Saving ACF field', key, val);
+
+        options.body.append(key, val);
+
+        key = key.replace(/^acf/, 'acf_blocks[' + $(form).data('block-id') + ']');
+        console.log('Saving ACF field', key, val);
+        options.body.append(key, val);
+      });
+    });
+
+    return next(options);
+  });
+
+  field_groups.forEach(function (group) {
+    var slug = 'acf/' + group.block_name;
+
+    console.log('Register block', slug, group);
+
+    wp.blocks.registerBlockType(slug, {
+      title: group.title,
+      description: group.description,
+      icon: group.block_icon || 'feedback',
+      category: 'widgets',
+      supports: {
+        html: false,
+      },
+      getEditWrapperProps: function( attributes ) {
+        return attributes;
+      },
+      edit: function (block) {
+        // console.log('edit', block);
+        var el = wp.element.createElement;
+
+        block.attributes.post_id = <?php echo json_encode(intval(get_the_ID())); ?>;
+        var block_id = block.attributes.block_id = block.attributes.block_id || block.clientId;
+
+        var remote = el('form', {
+          className: 'acf-block-group-content',
+          'data-block-id': block_id,
+        }, [
+          el(wp.components.ServerSideRender, {
+            block: slug,
+            attributes: {
+              acf_fields: '',
+              acf_field_group: JSON.stringify(group),
+              post_id: <?php echo json_encode(intval(get_the_ID())); ?>,
+              block_id: block_id,
+            },
+          })
+        ]);
+
+        var children = [];
+
+        if (group.style === 'default') {
+          children.push(el('div', { className: ['acf-block-group-heading'] }, [group.title]));
+        }
+
+        // setTimeout(function () {
+        //   acf.do_action('ready', $('[data-block-id="' + block_id + '"]'));
+        // }, 500);
+
+        children.push(remote);
+
+        return el('div', {}, children);
+      },
+      save: function () {
+        // console.log('SAVE');
+        return null;
+      },
+    })
+  });
 })(jQuery);
 </script>
+<style>
+.acf-block-group-heading {
+  background-color: #EEE;
+  padding: 0.25em 0.5em;
+  margin-left: -0.5em;
+  margin-right: -0.5em;
+}
+.acf-block-group-content {
+}
+</style>
 <?
 }, 0);

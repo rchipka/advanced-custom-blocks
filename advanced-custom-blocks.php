fa31b86b8ca1f7e0992a19b46c8c464241c1491b
Robbie Chipka <chipka01@email.franklin.edu>
2018-11-15 19:45:49 -0500

    Update advanced-custom-blocks.php

--- advanced-custom-blocks.php
+++ advanced-custom-blocks.php
@@ -4,7 +4,7 @@
 * Plugin Name: Advanced Custom Blocks
 * Plugin URI: https://github.com/rchipka/advanced-custom-blocks
 * Description: ACF for Gutenberg blocks
-* Version: 2.2.0
+* Version: 2.2.1
 * Author: Robbie Chipka
 * Author URI: https://github.com/rchipka`
 * GitHub Plugin URI: https://github.com/rchipka/advanced-custom-blocks
@@ -20,7 +20,7 @@ add_action('acf/include_field_types', function ($version) {
     return;
   }
 
-  include_once('acf-block-field.php');
+//   include_once('acf-block-field.php');
 }, 10, 1);
 
 function acb_current_block($key = null) {
